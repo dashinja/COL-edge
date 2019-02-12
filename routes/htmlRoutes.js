e70a75e807e1e4df7d73fe4a51783d5fe137668c
DaShinja <byron@dashinja.com>
2019-02-11 20:21:07 -0500

    middle of view setup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,29 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // oAuth
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    res.render("users", {
+      user: {
+        name: req.user.displayName,
+        image: req.user._json.image.url
+      }
     });
+    
+    // Load index page
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
