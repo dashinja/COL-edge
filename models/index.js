93f40d265fe5ace4e3cfc668cd2acda7989b5d46
mspenski <44811974+mspenski@users.noreply.github.com>
2019-02-12 21:13:30 -0500

    changes didn't work need to do more research

--- models/index.js
+++ models/index.js
@@ -1,28 +1,23 @@
 "use strict";
-//   "production": {
-//     "use_env_variable": "JAWSDB_URL",
-//     "dialect": "mysql"
-//   }
-// }
 
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "production";
+var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
-var db = { vfzsh48as7nymspt };
+var db = {};
 
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-var sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
-// }
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter(function(file) {
