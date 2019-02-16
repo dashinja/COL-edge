c25fd64eb14074f87e487a87702396c099f1470e
DaShinja <byron@dashinja.com>
2019-02-16 10:22:39 -0500

    profile displays more information:
    * Major with starting salary and mid-career salary
    * Living Place with cost of living and cost of living with rent in USD

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -29,9 +29,68 @@ module.exports = function(app) {
     if (!req.user) {
       res.redirect('/');
     } else {
-      res.render('profile', {
-        user: req.user
-      });
+      /*MAKE QUERIES TO GET FULL DATA DISPLAY OBJECTS FROM DATABASE BASED ON INFO HELD ON USER OBJECT */
+      console.log('req.user on profile page load');
+      console.log(req.user);
+      let userData = {
+        user: req.user,
+        major: '',
+        cost: ''
+      };
+
+      db.major
+        .findOne({
+          where: {
+            major: req.user.majorChoice
+          }
+        })
+        .then(fullMajorRow => {
+          console.log("\nI'm fullMajorRow");
+          console.log(fullMajorRow.dataValues);
+          userData.major = fullMajorRow.dataValues;
+          console.log("\n I'm userData with .major updated");
+          console.log(userData);
+
+          db.cost
+            .findOne({
+              where: {
+                city: req.user.cityChoice
+              }
+            })
+            .then(fullCostRow => {
+              console.log("\nI'm fullCostRow");
+              console.log(fullCostRow.dataValues);
+              // cli_including_rent to USD
+              fullCostRow.dataValues.cli_plus_rent = (
+                (parseInt(fullCostRow.dataValues.cli_plus_rent) / 100) *
+                57173
+              ).toFixed();
+
+              console.log("\nI'm calculated fullCostRow for cli_plus_rent");
+              console.log(fullCostRow.dataValues);
+
+              // cli to USD
+              fullCostRow.dataValues.cli = (
+                (parseInt(fullCostRow.dataValues.cli) / 100) *
+                57173
+              ).toFixed();
+
+              console.log("\nI'm calculated fullCostRow for cli");
+              console.log(fullCostRow.dataValues);
+
+              userData.cost = fullCostRow.dataValues;
+              console.log("\n I'm userData with .cost updated");
+              console.log(userData);
+              // res.render('profile', userData);
+            })
+            .then(() => {
+              console.log("\nI'm userData:");
+              console.log(userData);
+              res.render('profile', userData);
+            })
+            .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
     }
   });
 
