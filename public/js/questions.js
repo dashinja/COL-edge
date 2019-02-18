099e75481c67d0dd7841e728dd9a753147357c57
DaShinja <byron@dashinja.com>
2019-02-18 12:11:58 -0500

    separate out scripts on questions.handlebars and put scripts into separate file

---
+++ public/js/questions.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('#submitBTN').on('click', function(event) {
+    event.preventDefault();
+
+    let majorChoice = $('#a2 option:selected').text();
+    let cityChoice = $('#a3 option:selected').text();
+
+    let answers = {
+      major: majorChoice,
+      cost: cityChoice.split(',')[0]
+    };
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/user/answers',
+      data: answers,
+      success: response => {
+        return window.location.replace(response);
+      }
+    });
+  });
+});
