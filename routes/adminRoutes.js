d4d7f9a4b6321537b8a79881492a6b29ccd15e11
DaShinja <byron@dashinja.com>
2020-02-20 22:53:16 -0500

    convert adminRoutes.js to async/await

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,32 +1,29 @@
-const express = require('express');
-const adminRouter = express.Router();
-const db = require('../models');
+const express = require('express')
+const adminRouter = express.Router()
+const db = require('../models')
 
-adminRouter.route('/').get((req, res) => {
+adminRouter.route('/').get(async (req, res) => {
   if (!req.user.admin) {
-    res.redirect('/profile');
+    res.redirect('/profile')
   } else {
-    const key = req.user.username ? 'username' : 'localUsername';
-    const value = req.user.username || req.user.localUsername;
+    const key = req.user.username ? 'username' : 'localUsername'
+    const value = req.user.username || req.user.localUsername
 
-    db.user
-      .findOne({
-        where: {
-          [key]: value,
-        },
-      })
-      .then(found => {
-        db.testimonial.findAll({}).then(testimonials => {
-          db.indexTestimonial.findAll({}).then(indexTestimonials => {
-            res.render('admin', {
-              user: found.dataValues,
-              testimonials: testimonials,
-              indexTestimonials: indexTestimonials,
-            });
-          });
-        });
-      });
+    const user = db.user.findOne({
+      where: {
+        [key]: value,
+      },
+    })
+
+    const testimonials = db.testimonial.findAll({})
+    const indexTestimonials = db.indexTestimonial.findAll({})
+
+    res.render('admin', {
+      user: user.dataValues,
+      testimonials: testimonials,
+      indexTestimonials: indexTestimonials,
+    })
   }
-});
+})
 
-module.exports = adminRouter;
+module.exports = adminRouter
