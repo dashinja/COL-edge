4c0a6b0316504d6aeb542a262c512d32f5c36f00
DaShinja <byron@dashinja.com>
2020-02-20 23:15:10 -0500

    update adminRoutes.js to async/await

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,166 +3,166 @@ const apiRouter = express.Router()
 const db = require('../models')
 
 apiRouter.route('/user').get(async (req, res) => {
-  const key = req.user.username ? 'username' : 'localUsername'
-  const value = req.user.username || req.user.localUsername
+    const key = req.user.username ? 'username' : 'localUsername'
+    const value = req.user.username || req.user.localUsername
 
-  const user = await db.user.findOne({
-    where: {
-      [key]: value,
-    },
-  })
+    const user = await db.user.findOne({
+        where: {
+            [key]: value,
+        },
+    })
 
-  res.send(user)
+    res.send(user)
 })
 
 apiRouter.route('/cities').get(async (req, res) => {
-  const cities = await db.costOfLiving.findAll({})
-  res.send({ cities: cities.map(c => c.city) })
+    const cities = await db.costOfLiving.findAll({})
+
+    res.send({ cities: cities.map(c => c.city) })
 })
 
-apiRouter.route('/city/:cityname').get((req, res) => {
-  db.costOfLiving
-    .findOne({
-      where: {
-        city: req.params.cityname,
-      },
-    })
-    .then(city => {
-      res.send(city)
-    })
-})
-
-apiRouter.route('/user/answers').post((req, res, next) => {
-  let addUserChoice = {
-    majorChoice: req.body.major,
-    cityChoice: req.body.cost,
-  }
-  const key = req.user.username ? 'username' : 'localUsername'
-  const value = req.user.username || req.user.localUsername
-
-  db.user
-    .findOne({
-      where: {
-        [key]: value,
-      },
-    })
-    .then(user => {
-      if (user) {
-        db.user.update(addUserChoice, {
-          where: {
-            [key]: value,
-          },
-        })
-        res.json('/profile')
-      }
-    })
-    .catch(err => console.log(err))
-})
-
-apiRouter.route('/note').post((req, res) => {
-  const key = req.user.username ? 'username' : 'localUsername'
-  const value = req.user.username || req.user.localUsername
-
-  const noteInfo = {
-    [key]: value,
-    note: req.body.note,
-  }
-  if (!req.user) {
-    return
-  } else {
-    db.user
-      .findOne({
+apiRouter.route('/city/:cityname').get(async (req, res) => {
+    const city = await db.costOfLiving.findOne({
         where: {
-          [key]: value,
+            city: req.params.cityname,
         },
-      })
-      .then(user => {
-        if (user) {
-          db.note
-            .create(noteInfo)
-            .then(note => {})
-            .catch(e => console.log(e))
-        }
-      })
-  }
-})
+    })
 
-apiRouter.route('/chat').post((req, res) => {
-  db.chat.create(req.body).then(newMessage => {
-    res.send(newMessage)
-  })
+    res.send(city)
 })
 
 apiRouter
-  .route('/testimony/:dest')
-  .post((req, res) => {
-    const username = req.user.username || req.user.localUsername
-    const testimony = {
-      username,
-      testimonial: req.body.testimony,
-    }
-    req.user.picture ? (testimony.image = req.user.picture) : null
-    if (req.params.dest === 'index') {
-      db.indexTestimonial
-        .findOne({
-          where: {
-            username: req.body.username,
-          },
+    .route('/user/answers')
+    .post(async (req, res, next) => {
+        let addUserChoice = {
+            majorChoice: req.body.major,
+            cityChoice: req.body.cost,
+        }
+        const key = req.user.username ? 'username' : 'localUsername'
+        const value = req.user.username || req.user.localUsername
+
+        const user = await db.user.findOne({
+            where: {
+                [key]: value,
+            },
         })
-        .then(testimony => {
-          if (testimony) {
-            return
-          } else {
-            db.indexTestimonial.create(req.body).then(newIndexTestimony => {
-              const newInfo = {
-                onIndexPage: true,
-              }
-              db.testimonial
-                .update(newInfo, {
-                  where: {
-                    username: req.body.username,
-                  },
-                })
-                .then(testimonial => {
-                  res.send(newInfo)
-                })
+
+        if (user) {
+            await db.user.update(addUserChoice, {
+                where: {
+                    [key]: value,
+                },
             })
-          }
-        })
-    } else if (req.params.dest === 'user') {
-      db.testimonial.create(testimony).then(newTestimony => {
-        res.send(newTestimony)
-      })
+            res.json('/profile')
+        }
+    })
+    .catch(err => console.log(err))
+
+apiRouter.route('/note').post(async (req, res) => {
+    const key = req.user.username ? 'username' : 'localUsername'
+    const value = req.user.username || req.user.localUsername
+
+    const noteInfo = {
+        [key]: value,
+        note: req.body.note,
     }
-  })
-  .delete((req, res) => {
-    if (req.params.dest === 'index') {
-      db.indexTestimonial
-        .destroy({
-          where: {
-            username: req.body.username,
-          },
-        })
-        .then(removed => {
-          res.send('¡Lo hicimos!')
-        })
+
+    if (!req.user) {
+        return
     }
-  })
-  .patch((req, res) => {
-    if (req.params.dest === 'user') {
-      const newInfo = {
-        onIndexPage: req.body.onIndexPage,
-      }
-      db.testimonial
-        .update(newInfo, {
-          where: {
-            username: req.body.username,
-          },
+
+    else {
+        const user = await db.user.findOne({
+            where: {
+                [key]: value,
+            },
         })
-        .then(updated => {
-          res.send(updated)
-        })
+
+        if (user) {
+            await db.note
+                .create(noteInfo)
+                .then(note => { })
+                .catch(e => console.log(e))
+        }
     }
-  })
+})
+
+apiRouter.route('/chat').post(async (req, res) => {
+
+    const newMessage = await db.chat.create(req.body)
+
+    res.send(newMessage)
+
+})
+
+apiRouter
+    .route('/testimony/:dest')
+    .post(async (req, res) => {
+        const username = req.user.username || req.user.localUsername
+        const testimony = {
+            username,
+            testimonial: req.body.testimony,
+        }
+        req.user.picture ? (testimony.image = req.user.picture) : null
+        if (req.params.dest === 'index') {
+            const testimony = await db.indexTestimonial
+                .findOne({
+                    where: {
+                        username: req.body.username,
+                    },
+                })
+
+            if (testimony) {
+                return
+            } else {
+                const newIndexTestimony = await db.indexTestimonial.create(req.body)
+
+                const newInfo = {
+                    onIndexPage: true,
+                }
+
+                const testimonial = await db.testimonial
+                    .update(newInfo, {
+                        where: {
+                            username: req.body.username,
+                        },
+                    })
+
+                res.send(newInfo)
+            }
+        }
+        else if (req.params.dest === 'user') {
+            const newTestimony = await db.testimonial.create(testimony)
+
+            res.send(newTestimony)
+        }
+    })
+    .delete(async (req, res) => {
+        if (req.params.dest === 'index') {
+            await db.indexTestimonial
+                .destroy({
+                    where: {
+                        username: req.body.username,
+                    },
+                })
+
+            res.send('¡Lo hicimos!')
+        }
+    })
+    .patch(async (req, res) => {
+        if (req.params.dest === 'user') {
+            const newInfo = {
+                onIndexPage: req.body.onIndexPage,
+            }
+            const updated = await db.testimonial
+                .update(newInfo, {
+                    where: {
+                        username: req.body.username,
+                    },
+                })
+
+            res.send(updated)
+        }
+    })
 
 module.exports = apiRouter
