b4e61f5413d60706d10ba41a6fed7d1ebe51a9ae
DaShinja <byron@dashinja.com>
2020-02-20 23:23:37 -0500

    update htmlRoutes.js to async/await

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const htmlRouter = express.Router();
 const db = require('../models');
 
-htmlRouter.route('/').get((req, res) => {
-  db.indexTestimonial.findAll({}).then(testimonials => {
-    res.render('index', { user: req.user, testimonials: testimonials });
-  });
+htmlRouter.route('/').get(async (req, res) => {
+  const testimonials = await db.indexTestimonial.findAll({})
+
+  res.render('index', { user: req.user, testimonials: testimonials });
 });
 
 htmlRouter.route('/signUp').get((req, res) => {
@@ -21,38 +21,43 @@ htmlRouter.route('/error').get((req, res) => {
 });
 
 //Populates all the questions
-htmlRouter.route('/questions').get((req, res) => {
+htmlRouter.route('/questions').get(async (req, res) => {
   const regex = /(https?)(:\/\/)(.*)\//g;
   let leResult;
   req.headers.referer ? (leResult = req.headers.referer.replace(regex, '')) : null;
+
   if (!req.user) {
     res.redirect('/');
-  } else if (req.user.majorChoice && leResult !== 'profile') {
+  }
+
+  else if (req.user.majorChoice && leResult !== 'profile') {
     res.redirect('/profile');
-  } else {
+  }
+
+  else {
     let allQuestions = {
       major: '',
       cost: '',
     };
-    db.majorSalaries
-      .findAll({ attributes: ['major'] })
-      .then(results => {
-        allQuestions.major = results;
 
-        db.costOfLiving
-          .findAll({
-            attributes: ['city', 'state'],
-          })
-          .then(results => {
-            allQuestions.cost = results;
-            res.render('questions', {
-              user: req.user,
-              answers: allQuestions,
-            });
-          })
-          .catch(err => console.log(err));
-      })
+    const salaryResults = await db.majorSalaries
+      .findAll({ attributes: ['major'] })
       .catch(err => console.log(err));
+
+
+    allQuestions.major = salaryResults;
+
+    const colResults = await db.costOfLiving
+      .findAll({
+        attributes: ['city', 'state'],
+      }).catch(err => console.log(err));
+
+    allQuestions.cost = colResults;
+
+    res.render('questions', {
+      user: req.user,
+      answers: allQuestions,
+    });
   }
 });
 
